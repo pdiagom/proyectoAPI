55b6928ee98f23c64954aa4d7d5216161beb2094
David Galan Valle <david.galanv@eviden.com>
2024-05-24 09:05:58 +0200

    arreglo de error en github

--- Club-Nautico/src/main/java/com/eviden/pablo/api/clubNautico/Club/Nautico/repository/BarcoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eviden.pablo.api.clubNautico.Club.Nautico.repository;
-
-import com.eviden.pablo.api.clubNautico.Club.Nautico.entity.Barco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BarcoRepository extends JpaRepository<Barco,Long> {
-
-}
