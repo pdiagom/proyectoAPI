7c94c709b5bd3cf5ec782fdffbdb4bd2992827a9
Pablo Diaz Gomez <pablo.diaz@eviden.com>
2024-05-29 12:09:58 +0200

    Version 1.0

--- src/main/java/club_nautico/exception/dto/ErrorMessage.java
+++ src/main/java/club_nautico/exception/dto/ErrorMessage.java
@@ -6,12 +6,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ErrorMessage {
     private HttpStatus status;
     private String message;
 
-
+    public ErrorMessage(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
 }
